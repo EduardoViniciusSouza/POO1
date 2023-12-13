ff41e042bc69fc9ac1977045b7573c2657570113
EduardoViniciusSouza <eduardovinicius.macielsouza@gamail.com>
2023-12-13 18:13:45 -0300

    Refatorando os models com as funcionalidades do Lombok

--- src/main/java/com/eduardo/todosimple/models/Leads.java
+++ src/main/java/com/eduardo/todosimple/models/Leads.java
@@ -10,10 +10,19 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = Leads.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Leads {
   public static final String TABLE_NAME = "leads";
 
@@ -36,86 +45,5 @@ public class Leads {
   @Column(name = "description", length = 255, nullable = true)
   @Size(min = 0, max = 255)
   private String description;
-
-  public Leads() {
-  }
-
-  public Leads(Long id, User user, String foneNumber, String name, String description) {
-    this.id = id;
-    this.user = user;
-    this.foneNumber = foneNumber;
-    this.name = name;
-    this.description = description;
-  }
-
-  public Long getId() {
-    return this.id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public User getUser() {
-    return this.user;
-  }
-
-  public void setUser(User user) {
-    this.user = user;
-  }
-
-  public String getFoneNumber() {
-    return this.foneNumber;
-  }
-
-  public void setFoneNumber(String foneNumber) {
-    this.foneNumber = foneNumber;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getDescription() {
-    return this.description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == this)
-      return true;
-
-    if (obj == null) {
-      return false;
-    }
-    if (!(obj instanceof Leads)) {
-      return false;
-    }
-    Leads other = (Leads) obj;
-    if (this.id == null)
-      if (other.id == null)
-        return false;
-      else if (this.id.equals(other.id))
-        return false;
-    return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user)
-        && Objects.equals(this.foneNumber, other.foneNumber);
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + (this.id == null ? 0 : this.id.hashCode());
-    return result;
-
-  }
 
 }
--- src/main/java/com/eduardo/todosimple/models/User.java
+++ src/main/java/com/eduardo/todosimple/models/User.java
@@ -14,7 +14,11 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
@@ -22,6 +26,11 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 @Entity
 
 @Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class User {
 
   public interface CreateUser {
@@ -58,94 +67,5 @@ public class User {
   @OneToMany(mappedBy = "user")
   @JsonProperty(access = Access.WRITE_ONLY)
   private List<Leads> leads = new ArrayList<>();
-
-  public User() {
-  }
-
-  public User(Long id, String name, String email, String foneNumber, String password) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.foneNumber = foneNumber;
-    this.password = password;
-  }
-
-  public Long getId() {
-    return this.id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getEmail() {
-    return this.email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getFoneNumber() {
-    return this.foneNumber;
-  }
-
-  public void setFoneNumber(String foneNumber) {
-    this.foneNumber = foneNumber;
-  }
-
-  public String getPassword() {
-    return this.password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public List<Leads> getLeads() {
-    return this.leads;
-  }
-
-  public void setLeads(List<Leads> leads) {
-    this.leads = leads;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == this)
-      return true;
-
-    if (obj == null) {
-      return false;
-    }
-    if (!(obj instanceof User)) {
-      return false;
-    }
-    User other = (User) obj;
-    if (this.id == null)
-      if (other.id == null)
-        return false;
-      else if (this.id.equals(other.id))
-        return false;
-    return Objects.equals(this.id, other.id) && Objects.equals(this.email, other.email)
-        && Objects.equals(this.password, other.password);
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + (this.id == null ? 0 : this.id.hashCode());
-    return result;
-
-  }
 
 }
